c06173d78abae217a4efcd8d83e62b573878897d
Nettune057 <nettune057@gmail.com>
2024-12-15 04:54:45 +0000

    First Commit

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,6 +7,14 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/items/")
+def create_item(item: dict):
+    return item
 
 if __name__ == "__main__":
     app.run(debug=True)
